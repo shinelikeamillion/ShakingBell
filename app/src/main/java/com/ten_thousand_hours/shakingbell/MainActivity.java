ebea4d0decb75b701a87d2eb7bdabc013726e0b9
ten_thousand_hours <geeks.liu@gmail.com>
2016-08-07 00:17:40 +0800

    Initial commit

---
+++ app/src/main/java/com/ten_thousand_hours/shakingbell/MainActivity.java
@@ -0,0 +1,28 @@
+package com.ten_thousand_hours.shakingbell;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final ShakingBell shakingBell = (ShakingBell) findViewById(R.id.shakeBell);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shakingBell.shake(1);
+            }
+        });
+    }
+}
